6accd86ecf9f110d1715be3df266cba42ee3dc07
rorshaksama <cmd.rorshak@laposte.net>
2020-03-04 11:52:33 +0100

    Ajout du back connection

--- Tpnote/src/main/java/fr/solutec/dao/PersonRepositary.java
+++ Tpnote/src/main/java/fr/solutec/dao/PersonRepositary.java
@@ -1,12 +1,16 @@
 package fr.solutec.dao;
 
-import java.util.List;
+
+import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 
 import fr.solutec.entities.Person;
 
 public interface PersonRepositary extends CrudRepository<Person,Long> {
-	public List<Person> findByLogin(String login);
-	public List<Person> findByMdp(String mdp);
+	
+	public Optional<Person> findByLoginAndMdp(String login,String mdp);
+	
+	
+	
 }
--- Tpnote/src/main/java/fr/solutec/rest/PersonRest.java
+++ Tpnote/src/main/java/fr/solutec/rest/PersonRest.java
@@ -1,10 +1,12 @@
 package fr.solutec.rest;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,16 +18,28 @@ import fr.solutec.entities.Person;
 public class PersonRest {
 	
 	@Autowired
-	private PersonRepositary personrepo;
+	private PersonRepositary personRepo;
 	
-	@RequestMapping(value = "/personl/{login}", method = RequestMethod.GET)
-	public List<Person> getLogin(@PathVariable String login){
-		return personrepo.findByLogin(login);
-	}
 	
-	@RequestMapping(value = "/personm/{mdp}", method = RequestMethod.GET)
-	public List<Person> getMdp(@PathVariable String mdp){
-		return personrepo.findByMdp(mdp);
+	@RequestMapping(value = "/person", method = RequestMethod.POST)
+	public Person getConnexion(@RequestBody Person p) {
+		
+		Optional<Person> person = personRepo.findByLoginAndMdp(p.getLogin(), p.getMdp());
+		
+		Person pe = new Person();
+		
+		if(person.isPresent()) {
+			
+			pe = person.get();
+			pe.setMdp("");
+		}
+		
+		return pe;
 	}
+			
+	
+	
+	
+	
 	
 }
